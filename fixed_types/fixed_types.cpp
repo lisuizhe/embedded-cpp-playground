93df60677b8b5f676429df457d18ff1fc2d7bd28
lisuizhe <suizhe.li@outlook.com>
2021-12-21 18:52:30 +0900

    add some exercises

---
+++ fixed_types/fixed_types.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void SendDataToDevice(void* buffer, uint32_t size) {
+  std::cout << "Sending data chunk of size " << size << std::endl;
+}
+
+int main() {
+  char buffer[] = "Hello, world!";
+  uint32_t size = sizeof(buffer);
+  SendDataToDevice(&size, sizeof(size));
+  SendDataToDevice(buffer, size);
+  return 0;
+}
